e3dcd0a460b94df3ad37213512f57094cf0adcb0
Mikolaj Przybylski <clovers02123@gmail.com>
2023-12-03 20:57:17 +0100

    Lights: Increase default ambient value

--- src/Light.h
+++ src/Light.h
@@ -10,7 +10,7 @@ public:
     static std::shared_ptr<Light> create();
     Light() = default;
 
-    glm::vec3 ambient = glm::vec3(0.1f, 0.1f, 0.1f);
+    glm::vec3 ambient = glm::vec3(0.2f, 0.2f, 0.2f);
     glm::vec3 diffuse = glm::vec3(1.0f, 1.0f, 1.0f);
     glm::vec3 specular = glm::vec3(1.0f, 1.0f, 1.0f);
 };
