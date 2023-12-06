306d886f03894664b810b213c28c18940d952cf9
Mikolaj Przybylski <clovers02123@gmail.com>
2023-12-06 19:56:52 +0100

    Drawable: Fix conflicting argument types in ctor declaration/definition

--- src/Drawable.h
+++ src/Drawable.h
@@ -7,7 +7,7 @@ class Drawable : public Component
 {
 public:
     explicit Drawable(std::shared_ptr<Material> const& material);
-    explicit Drawable(std::shared_ptr<Material> const& material, int32_t render_order);
+    explicit Drawable(std::shared_ptr<Material> const& material, int32_t const render_order);
     ~Drawable() override = default;
     virtual void initialize() override;
     virtual void draw() const = 0;
