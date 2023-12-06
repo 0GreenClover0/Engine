7b9fd8a135696a7a7abadd02603e332b312d7508
Mikolaj Przybylski <clovers02123@gmail.com>
2023-12-06 11:14:31 +0100

    Main: Remove unused load_texture()
    We have that function in Model already.

--- src/main.cpp
+++ src/main.cpp
@@ -352,35 +352,3 @@ void focus_callback(GLFWwindow* window, int const focused)
     if (focused == 0)
         mouse_just_entered = true;
 }
-
-unsigned int generate_texture(char const* path)
-{
-    unsigned int texture_id;
-    glGenTextures(1, &texture_id);
-    glBindTexture(GL_TEXTURE_2D, texture_id);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    stbi_set_flip_vertically_on_load(true);
-
-    int width, height, channel_count;
-    unsigned char* data = stbi_load(path, &width, &height, &channel_count, 0);
-
-    if (data == nullptr)
-    {
-        std::cout << "Warning. Failed to load a texutre.";
-    }
-    else
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-
-    stbi_image_free(data);
-
-    return texture_id;
-}
