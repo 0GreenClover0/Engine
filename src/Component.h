33b0376e416f89604b3bd7105cdcfe06e7c9e687
Mikolaj Przybylski <clovers02123@gmail.com>
2024-02-18 23:54:10 +0100

    Component: Remove obsolete FIXMEs about Awake, Start etc. call order

--- src/Component.h
+++ src/Component.h
@@ -8,8 +8,7 @@ class Component : public std::enable_shared_from_this<Component>
 {
 public:
     virtual ~Component() = default;
-    // TODO: Call these on entities instantiated during these calls.
-    // TODO: Order of calling these on entities instantiated during Awake is wrong.
+
     virtual void initialize();
     virtual void awake();
     virtual void start();
