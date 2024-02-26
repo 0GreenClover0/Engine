904908eb826902045ee26635470f31255f258f27
Mikolaj Przybylski <clovers02123@gmail.com>
2024-02-26 15:05:50 +0100

    TextureLoader: Make TextureLoader more general
    load_texture() now takes an optional TextureSettings parameter
    and returns more info about the texture - width, height, component num.
    Add load_cubemap() function.

--- src/TextureLoader.h
+++ src/TextureLoader.h
@@ -2,15 +2,25 @@
 
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "Texture.h"
+
+struct TextureData
+{
+    std::uint32_t id = 0;
+    std::uint32_t width = 0;
+    std::uint32_t height = 0;
+    std::uint32_t number_of_components = 0;
+};
 
 class TextureLoader
 {
 public:
     virtual ~TextureLoader() = default;
 
-    Texture load_texture(std::string const& path, TextureType type);
+    [[nodiscard]] Texture load_texture(std::string const& path, TextureType const type, TextureSettings const& settings = {});
+    [[nodiscard]] Texture load_cubemap(std::vector<std::string> const& paths, TextureType const type, TextureSettings const& settings = {});
 
     static std::shared_ptr<TextureLoader> get_instance()
     {
@@ -26,5 +36,6 @@ protected:
 private:
     inline static std::shared_ptr<TextureLoader> instance;
 
-    std::uint32_t virtual texture_from_file(std::string const& path) = 0;
+    TextureData virtual texture_from_file(std::string const& path, TextureSettings const settings) = 0;
+    TextureData virtual cubemap_from_files(std::vector<std::string> const& paths, TextureSettings const settings) = 0;
 };
