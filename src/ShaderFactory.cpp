039054ca3c464fbd4dbcd4ec6b03eac98b97d424
Mikolaj Przybylski <clovers02123@gmail.com>
2024-03-16 19:21:58 +0100

    ShaderDX11: Flesh out ShaderDX11, use it in shader factory functions

--- src/ShaderFactory.cpp
+++ src/ShaderFactory.cpp
@@ -1,17 +1,31 @@
 #include "ShaderFactory.h"
 
+#include <iostream>
+
 #include "Renderer.h"
+#include "ShaderDX11.h"
 #include "ShaderGL.h"
 
 std::shared_ptr<Shader> ShaderFactory::create(std::string const& compute_path)
 {
-    if (Renderer::renderer_api == Renderer::RendererApi::OpenGL)
+    switch (Renderer::renderer_api)
     {
-        auto shader = std::make_shared<ShaderGL>(AK::Badge<ShaderFactory> {}, compute_path);
+    case Renderer::RendererApi::OpenGL:
+        {
+            auto shader = std::make_shared<ShaderGL>(AK::Badge<ShaderFactory> {}, compute_path);
 
-        // Since this is a compute shader we don't need to register it, as we only use registered shaders for drawables
+            // Since this is a compute shader we don't need to register it, as we only use registered shaders for drawables
 
-        return shader;
+            return shader;
+        }
+    case Renderer::RendererApi::DirectX11:
+        {
+            auto shader = std::make_shared<ShaderDX11>(AK::Badge<ShaderFactory> {}, compute_path);
+
+            // Since this is a compute shader we don't need to register it, as we only use registered shaders for drawables
+
+            return shader;
+        }
     }
 
     std::unreachable();
@@ -19,13 +33,24 @@ std::shared_ptr<Shader> ShaderFactory::create(std::string const& compute_path)
 
 std::shared_ptr<Shader> ShaderFactory::create(std::string const& vertex_path, std::string const& fragment_path)
 {
-    if (Renderer::renderer_api == Renderer::RendererApi::OpenGL)
+    switch (Renderer::renderer_api)
     {
-        auto shader = std::make_shared<ShaderGL>(AK::Badge<ShaderFactory> {}, vertex_path, fragment_path);
+    case Renderer::RendererApi::OpenGL:
+        {
+            auto shader = std::make_shared<ShaderGL>(AK::Badge<ShaderFactory> {}, vertex_path, fragment_path);
 
-        Renderer::get_instance()->register_shader(shader);
+            Renderer::get_instance()->register_shader(shader);
 
-        return shader;
+            return shader;
+        }
+    case Renderer::RendererApi::DirectX11:
+        {
+            auto shader = std::make_shared<ShaderDX11>(AK::Badge<ShaderFactory> {}, vertex_path, fragment_path);
+
+            Renderer::get_instance()->register_shader(shader);
+
+            return shader;
+        }
     }
 
     std::unreachable();
@@ -34,13 +59,24 @@ std::shared_ptr<Shader> ShaderFactory::create(std::string const& vertex_path, st
 std::shared_ptr<Shader> ShaderFactory::create(std::string const& vertex_path, std::string const& fragment_path,
                                               std::string const& geometry_path)
 {
-    if (Renderer::renderer_api == Renderer::RendererApi::OpenGL)
+    switch (Renderer::renderer_api)
     {
-        auto shader = std::make_shared<ShaderGL>(AK::Badge<ShaderFactory> {}, vertex_path, fragment_path, geometry_path);
+    case Renderer::RendererApi::OpenGL:
+        {
+            auto shader = std::make_shared<ShaderGL>(AK::Badge<ShaderFactory> {}, vertex_path, fragment_path, geometry_path);
 
-        Renderer::get_instance()->register_shader(shader);
+            Renderer::get_instance()->register_shader(shader);
 
-        return shader;
+            return shader;
+        }
+    case Renderer::RendererApi::DirectX11:
+        {
+            auto shader = std::make_shared<ShaderDX11>(AK::Badge<ShaderFactory> {}, vertex_path, fragment_path, geometry_path);
+
+            Renderer::get_instance()->register_shader(shader);
+
+            return shader;
+        }
     }
 
     std::unreachable();
@@ -50,15 +86,28 @@ std::shared_ptr<Shader> ShaderFactory::create(std::string const& vertex_path,
                                               std::string const& tessellation_control_path, std::string const& tessellation_evaluation_path,
                                               std::string const& fragment_path)
 {
-    if (Renderer::renderer_api == Renderer::RendererApi::OpenGL)
+    switch (Renderer::renderer_api)
     {
-        auto shader = std::make_shared<ShaderGL>(
-            AK::Badge<ShaderFactory> {}, vertex_path, tessellation_control_path, tessellation_evaluation_path, fragment_path
-        );
+    case Renderer::RendererApi::OpenGL:
+        {
+            auto shader = std::make_shared<ShaderGL>(
+                AK::Badge<ShaderFactory> {}, vertex_path, tessellation_control_path, tessellation_evaluation_path, fragment_path
+            );
 
-        Renderer::get_instance()->register_shader(shader);
+            Renderer::get_instance()->register_shader(shader);
 
-        return shader;
+            return shader;
+        }
+    case Renderer::RendererApi::DirectX11:
+        {
+            auto shader = std::make_shared<ShaderDX11>(
+                AK::Badge<ShaderFactory> {}, vertex_path, tessellation_control_path, tessellation_evaluation_path, fragment_path
+            );
+
+            Renderer::get_instance()->register_shader(shader);
+
+            return shader;
+        }
     }
 
     std::unreachable();
