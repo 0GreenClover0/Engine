4b464afb376fe9374ac6682dc249507416be8d06
Mikolaj Przybylski <clovers02123@gmail.com>
2024-03-16 18:39:53 +0100

    Window+Engine: Move settings to the Window's constructor
    + expose the native WIN32 functions for DX11.

--- src/Window.h
+++ src/Window.h
@@ -1,15 +1,19 @@
 #pragma once
 
+#include <d3d11.h>
+
 #include "AK/Types.h"
 #include "GLFW/glfw3.h"
+#include "Renderer.h"
 
 class Window
 {
 public:
-    Window(i32 screen_width, i32 screen_height, i32 const subsamples = 0);
+    Window(Renderer::RendererApi const renderer_api, i32 const screen_width, i32 const screen_height, i32 const subsamples = 0, bool const enable_vsync = false, bool const enable_mouse_capture = true);
     Window() = delete;
 
     [[nodiscard]] GLFWwindow* get_glfw_window() const;
+    [[nodiscard]] HWND get_win32_window() const;
 
 private:
     GLFWwindow* m_window;
