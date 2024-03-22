39878b47bfcaf08e0498ac60179d28b70fe926a6
Armez <zamylosz@gmail.com>
2024-03-22 20:33:13 +0100

    ShaderDX11: Add simple Constant Buffers for DX11 shaders

--- src/ShaderDX11.cpp
+++ src/ShaderDX11.cpp
@@ -91,9 +91,13 @@ ShaderDX11::ShaderDX11(AK::Badge<ShaderFactory>, std::string const& vertex_path,
     }
 
     {
-        std::array<D3D11_INPUT_ELEMENT_DESC, 1> constexpr input_element_desc =
+        std::array<D3D11_INPUT_ELEMENT_DESC, 3> constexpr input_element_desc =
         {
-            {{"POS", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0}}
+            {
+                { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+                { "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+                { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+            }
         };
 
         HRESULT const h_result = RendererDX11::get_instance_dx11()->get_device()->CreateInputLayout(input_element_desc.data(), input_element_desc.size(), vs_blob->GetBufferPointer(), vs_blob->GetBufferSize(), &m_input_layout);
