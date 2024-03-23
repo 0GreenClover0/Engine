7be741be895bb83e3e5cb699908af78326c2ca58
Mikolaj Przybylski <clovers02123@gmail.com>
2024-03-23 14:14:39 +0100

    Renderer: Abstract away setting shader uniforms

--- src/RendererDX11.h
+++ src/RendererDX11.h
@@ -22,7 +22,12 @@ public:
 
     [[nodiscard]] ID3D11Device* get_device() const;
     [[nodiscard]] ID3D11DeviceContext* get_device_context() const;
-    
+
+protected:
+    virtual void update_shader(std::shared_ptr<Shader> const& shader, glm::mat4 const& projection_view, glm::mat4 const& projection_view_no_translation) const override;
+    virtual void update_material(std::shared_ptr<Material> const& material) const override;
+    virtual void update_object(std::shared_ptr<Drawable> const& drawable, std::shared_ptr<Material> const& material, glm::mat4 const& projection_view) const override;
+
 private:
     virtual void initialize_global_renderer_settings() override;
     virtual void initialize_buffers(size_t const max_size) override;
--- src/RendererGL.cpp
+++ src/RendererGL.cpp
@@ -7,6 +7,7 @@
 #include "Entity.h"
 #include "MeshGL.h"
 #include "ShaderFactory.h"
+#include "Skybox.h"
 #include "TextureLoaderGL.h"
 
 std::shared_ptr<RendererGL> RendererGL::create()
@@ -45,6 +46,100 @@ void RendererGL::begin_frame() const
 
     glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void RendererGL::update_shader(std::shared_ptr<Shader> const& shader, glm::mat4 const& projection_view, glm::mat4 const& projection_view_no_translation) const
+{
+    // TODO: Ultimately we would probably want to cache the uniform location instead of retrieving them by name
+
+    shader->set_vec3("cameraPosition", Camera::get_main_camera()->get_position());
+    shader->set_mat4("PV", projection_view);
+    shader->set_mat4("PVnoTranslation", projection_view_no_translation);
+
+    // TODO: Choose only the closest lights
+
+    i32 enabled_light_count = 0;
+    for (u32 i = 0; i < m_point_lights.size(); ++i)
+    {
+        if (!m_point_lights[i]->enabled)
+            continue;
+
+        std::string light_element = std::format("pointLights[{}].", enabled_light_count);
+        shader->set_vec3(light_element + "position", m_point_lights[i]->entity->transform->get_local_position());
+
+        shader->set_vec3(light_element + "ambient", m_point_lights[i]->ambient);
+        shader->set_vec3(light_element + "diffuse", m_point_lights[i]->diffuse);
+        shader->set_vec3(light_element + "specular", m_point_lights[i]->specular);
+
+        shader->set_float(light_element + "constant", m_point_lights[i]->constant);
+        shader->set_float(light_element + "linear", m_point_lights[i]->linear);
+        shader->set_float(light_element + "quadratic", m_point_lights[i]->quadratic);
+
+        enabled_light_count++;
+    }
+
+    shader->set_int("pointLightCount", enabled_light_count > m_max_point_lights ? m_max_point_lights : enabled_light_count);
+
+    enabled_light_count = 0;
+    for (u32 i = 0; i < m_spot_lights.size(); ++i)
+    {
+        if (!m_spot_lights[i]->enabled)
+            continue;
+
+        std::string light_element = std::format("spotLights[{}].", enabled_light_count);
+        shader->set_vec3(light_element + "position", m_spot_lights[i]->entity->transform->get_local_position());
+        shader->set_vec3(light_element + "direction", m_spot_lights[i]->entity->transform->get_forward());
+
+        shader->set_vec3(light_element + "ambient", m_spot_lights[i]->ambient);
+        shader->set_vec3(light_element + "diffuse", m_spot_lights[i]->diffuse);
+        shader->set_vec3(light_element + "specular", m_spot_lights[i]->specular);
+
+        shader->set_float(light_element + "cutOff", m_spot_lights[i]->cut_off);
+        shader->set_float(light_element + "outerCutOff", m_spot_lights[i]->outer_cut_off);
+
+        shader->set_float(light_element + "constant", m_spot_lights[i]->constant);
+        shader->set_float(light_element + "linear", m_spot_lights[i]->linear);
+        shader->set_float(light_element + "quadratic", m_spot_lights[i]->quadratic);
+
+        enabled_light_count++;
+    }
+
+    shader->set_int("spotLightCount", enabled_light_count > m_max_spot_lights ? m_max_spot_lights : enabled_light_count);
+
+    bool const directional_light_on = m_directional_light != nullptr && m_directional_light->enabled;
+    if (directional_light_on)
+    {
+        shader->set_vec3("directionalLight.direction", m_directional_light->entity->transform->get_forward());
+
+        shader->set_vec3("directionalLight.ambient", m_directional_light->ambient);
+        shader->set_vec3("directionalLight.diffuse", m_directional_light->diffuse);
+        shader->set_vec3("directionalLight.specular", m_directional_light->specular);
+    }
+
+    shader->set_bool("directionalLightOn", directional_light_on);
+}
+
+void RendererGL::update_material(std::shared_ptr<Material> const& material) const
+{
+    material->shader->set_vec3("material.color", glm::vec3(material->color.x, material->color.y, material->color.z));
+    material->shader->set_float("material.specular", material->specular);
+    material->shader->set_float("material.shininess", material->shininess);
+
+    material->shader->set_float("radiusMultiplier", material->radius_multiplier);
+    material->shader->set_int("sector_count", material->sector_count);
+    material->shader->set_int("stack_count", material->stack_count);
+}
+
+void RendererGL::update_object(std::shared_ptr<Drawable> const& drawable, std::shared_ptr<Material> const& material, glm::mat4 const& projection_view) const
+{
+    if (material->needs_view_model)
+        material->shader->set_mat4("VM", Camera::get_main_camera()->get_view_matrix() * drawable->entity->transform->get_model_matrix());
+
+    if (material->needs_skybox)
+        Skybox::get_instance()->bind();
+
+    material->shader->set_mat4("PVM", projection_view * drawable->entity->transform->get_model_matrix());
+    material->shader->set_mat4("model", drawable->entity->transform->get_model_matrix());
 }
 
 void RendererGL::initialize_global_renderer_settings()
