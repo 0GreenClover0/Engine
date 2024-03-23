4e615956efc8b5b252cf586f106b8030f45d82fe
Mikolaj Przybylski <clovers02123@gmail.com>
2024-03-23 14:14:39 +0100

    Renderer: Abstract away setting shader uniforms

--- src/Renderer.h
+++ src/Renderer.h
@@ -69,6 +69,10 @@ protected:
         m_instance = renderer;
     }
 
+    void virtual update_shader(std::shared_ptr<Shader> const& shader, glm::mat4 const& projection_view, glm::mat4 const& projection_view_no_translation) const = 0;
+    void virtual update_material(std::shared_ptr<Material> const& material) const = 0;
+    void virtual update_object(std::shared_ptr<Drawable> const& drawable, std::shared_ptr<Material> const& material, glm::mat4 const& projection_view) const = 0;
+
     void virtual initialize_global_renderer_settings() = 0;
     void virtual initialize_buffers(size_t const max_size) = 0;
     void virtual perform_frustum_culling(std::shared_ptr<Material> const& material) const = 0;
@@ -77,15 +81,19 @@ protected:
 
     bool vsync_enabled = false;
 
-private:
-    void draw(std::shared_ptr<Material> const& material, glm::mat4 const& projection_view) const;
-    void draw_instanced(std::shared_ptr<Material> const& material, glm::mat4 const& projection_view, glm::mat4 const& projection_view_no_translation) const;
-
-    void set_shader_uniforms(std::shared_ptr<Shader> const& shader, glm::mat4 const& projection_view, glm::mat4 const& projection_view_no_translation) const;
-
+protected:
     inline static std::vector<std::shared_ptr<PointLight>> m_point_lights = {};
     inline static std::vector<std::shared_ptr<SpotLight>> m_spot_lights = {};
     inline static std::shared_ptr<DirectionalLight> m_directional_light = {};
+
+    // TODO: Retrieve this information from the shader
+    // NOTE: This has to be the same value as the variable in a shader to work in all cases.
+    i32 m_max_point_lights = 4;
+    i32 m_max_spot_lights = 4;
+
+private:
+    void draw(std::shared_ptr<Material> const& material, glm::mat4 const& projection_view) const;
+    void draw_instanced(std::shared_ptr<Material> const& material, glm::mat4 const& projection_view, glm::mat4 const& projection_view_no_translation) const;
 
     struct MaterialWithOrder
     {
@@ -103,9 +111,4 @@ private:
     std::vector<std::shared_ptr<Material>> m_instanced_materials = {};
 
     std::multiset<MaterialWithOrder> m_custom_render_order_materials = {};
-
-    // TODO: Retrieve this information from the shader
-    // NOTE: This has to be the same value as the variable in a shader to work in all cases.
-    i32 m_max_point_lights = 4;
-    i32 m_max_spot_lights = 4;
 };
