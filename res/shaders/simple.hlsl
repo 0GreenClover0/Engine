8cd964b84841a697a4dc2affd9b69fc272c21b9a
umbc1ok <umbcio@gmail.com>
2024-03-24 03:13:54 +0100

    ShaderDX11: Correct input layout

--- res/shaders/simple.hlsl
+++ res/shaders/simple.hlsl
@@ -6,24 +6,30 @@ cbuffer mycBuffer : register(b0)
 struct VS_Input
 {
     float3 pos: POSITION;
-    float2 texcoord : TEXCOORD;
     float3 normal : NORMAL;
+    float2 UV : TEXCOORD;
 };
 
 struct VS_Output
 {
-    float4 position : SV_POSITION;
+    float4 pos : SV_POSITION;
+    float2 UV : TEXCOORD;
+    float3 normal : NORMAL;
 };
+
+Texture2D ObjTexture;
+SamplerState ObjSamplerState;
 
 VS_Output vs_main(VS_Input input)
 {
     VS_Output output;
-    output.position = mul(projection_view, float4(input.pos.xyz, 1.0f));
-
+    output.pos = mul(projection_view, float4(input.pos.xyz, 1.0f));
+    output.normal = mul(input.normal,input.pos);
+    output.UV = input.UV;
     return output;
 }
 
 float4 ps_main(VS_Output input) : SV_TARGET
 {
-    return float4(1.0f, 1.0f, 1.0f, 1.0f);
+    return ObjTexture.Sample(ObjSamplerState, input.UV);
 }
