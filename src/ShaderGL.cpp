b0151a3b052105e73d314a052f0df8140a70d3bf
umbc1ok <umbcio@gmail.com>
2024-04-07 02:29:16 +0200

    ShaderDX11: Add hot-reload of shaders

--- src/ShaderGL.cpp
+++ src/ShaderGL.cpp
@@ -248,3 +248,7 @@ void ShaderGL::set_mat4(std::string const& name, glm::mat4 const value) const
 {
     glUniformMatrix4fv(glGetUniformLocation(program_id, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
 }
+
+void ShaderGL::load_shader()
+{
+}
