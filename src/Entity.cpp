0cf9c12f2be7ef882ef92abac33987966fd4d33d
Mikolaj Przybylski <clovers02123@gmail.com>
2024-04-14 22:28:20 +0200

    Entity: Remove wrong comments about returning shared_ptrs by create()
    This is perfectly fine.

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -7,8 +7,6 @@ Entity::Entity(AK::Badge<Entity>, std::string name) : name(std::move(name))
 {
 }
 
-// This should return a weak_ptr but it's a pain to use this in that way
-// Maybe there is a better pattern
 std::shared_ptr<Entity> Entity::create(std::string const& name)
 {
     auto entity = std::make_shared<Entity>(AK::Badge<Entity> {}, name);
@@ -20,8 +18,6 @@ std::shared_ptr<Entity> Entity::create(std::string const& name)
     return entity;
 }
 
-// This should return a weak_ptr but it's a pain to use this in that way
-// Maybe there is a better pattern
 std::shared_ptr<Entity> Entity::create(std::string const& guid, std::string const& name)
 {
     auto entity = std::make_shared<Entity>(AK::Badge<Entity> {}, name);
