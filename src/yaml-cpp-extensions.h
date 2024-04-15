ec4e78f5f5c97b3d1b5ef06a04978ce9d38b4bbf
Armez <zamylosz@gmail.com>
2024-04-15 17:42:38 +0200

    Serialization: Add vec2 and wstring yaml operators

--- src/yaml-cpp-extensions.h
+++ src/yaml-cpp-extensions.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <xstring>
+#include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
 #include <yaml-cpp/emitter.h>
@@ -7,6 +9,28 @@
 
 namespace YAML
 {
+    template<>
+    struct convert<glm::vec2>
+    {
+        static Node encode(glm::vec2 const& rhs)
+        {
+            Node node;
+            node.push_back(rhs.x);
+            node.push_back(rhs.y);
+            return node;
+        }
+
+        static bool decode(Node const& node, glm::vec2& rhs)
+        {
+            if (!node.IsSequence() || node.size() != 2)
+                return false;
+
+            rhs.x = node[0].as<float>();
+            rhs.y = node[1].as<float>();
+            return true;
+        }
+    };
+
     template<>
     struct convert<glm::vec3>
     {
@@ -56,6 +80,34 @@ namespace YAML
             return true;
         }
     };
+
+    template<>
+    struct convert<std::wstring>
+    {
+        static Node encode(std::wstring const& rhs)
+        {
+            std::string const narrow(rhs.begin(), rhs.end());
+            return Node(narrow);
+        }
+
+        static bool decode(Node const& node, std::wstring& rhs)
+        {
+            if (!node.IsScalar()) {
+                return false;
+            }
+
+            const std::string& narrow = node.as<std::string>();
+            rhs = std::wstring(narrow.begin(), narrow.end());
+            return true;
+        }
+    };
+}
+
+YAML::Emitter& operator<<(YAML::Emitter& out, glm::vec2 const& v)
+{
+    out << YAML::Flow;
+    out << YAML::BeginSeq << v.x << v.y << YAML::EndSeq;
+    return out;
 }
 
 YAML::Emitter& operator<<(YAML::Emitter& out, glm::vec3 const& v)
@@ -69,5 +121,12 @@ YAML::Emitter& operator<<(YAML::Emitter& out, glm::vec4 const& v)
 {
     out << YAML::Flow;
     out << YAML::BeginSeq << v.x << v.y << v.z << v.w << YAML::EndSeq;
+    return out;
+}
+
+YAML::Emitter& operator<<(YAML::Emitter& out, std::wstring const& v)
+{
+    std::string const narrow(v.begin(), v.end());
+    out << narrow;
     return out;
 }
