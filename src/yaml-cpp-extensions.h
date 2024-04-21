0ac691b7b834b43efc3784963ef9a31d09c48ba4
Mikolaj Przybylski <clovers02123@gmail.com>
2024-04-21 19:07:08 +0200

    Serialization: Add 'inline' specifier to all custom YAML operators

--- src/yaml-cpp-extensions.h
+++ src/yaml-cpp-extensions.h
@@ -165,35 +165,35 @@ namespace YAML
     };
 }
 
-YAML::Emitter& operator<<(YAML::Emitter& out, glm::vec2 const& v)
+inline YAML::Emitter& operator<<(YAML::Emitter& out, glm::vec2 const& v)
 {
     out << YAML::Flow;
     out << YAML::BeginSeq << v.x << v.y << YAML::EndSeq;
     return out;
 }
 
-YAML::Emitter& operator<<(YAML::Emitter& out, glm::vec3 const& v)
+inline YAML::Emitter& operator<<(YAML::Emitter& out, glm::vec3 const& v)
 {
     out << YAML::Flow;
     out << YAML::BeginSeq << v.x << v.y << v.z << YAML::EndSeq;
     return out;
 }
 
-YAML::Emitter& operator<<(YAML::Emitter& out, glm::vec4 const& v)
+inline YAML::Emitter& operator<<(YAML::Emitter& out, glm::vec4 const& v)
 {
     out << YAML::Flow;
     out << YAML::BeginSeq << v.x << v.y << v.z << v.w << YAML::EndSeq;
     return out;
 }
 
-YAML::Emitter& operator<<(YAML::Emitter& out, std::wstring const& v)
+inline YAML::Emitter& operator<<(YAML::Emitter& out, std::wstring const& v)
 {
     std::string const narrow(v.begin(), v.end());
     out << narrow;
     return out;
 }
 
-YAML::Emitter& operator<<(YAML::Emitter& out, std::shared_ptr<Shader> const& shader)
+inline YAML::Emitter& operator<<(YAML::Emitter& out, std::shared_ptr<Shader> const& shader)
 {
     out << YAML::BeginMap; // Shader
 
@@ -206,7 +206,7 @@ YAML::Emitter& operator<<(YAML::Emitter& out, std::shared_ptr<Shader> const& sha
     return out;
 }
 
-YAML::Emitter& operator<<(YAML::Emitter& out, std::shared_ptr<Material> const& material)
+inline YAML::Emitter& operator<<(YAML::Emitter& out, std::shared_ptr<Material> const& material)
 {
     out << YAML::BeginMap; // Material
 
