125d235f71c51264842f0022667a063cdfebf85a
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2024-04-28 17:26:53 +0200

    Collisions: Add simple OBB rect x rect detection
    No solving yet. Patience, my padawan.

--- src/Collider2D.cpp
+++ src/Collider2D.cpp
@@ -22,6 +22,12 @@ std::shared_ptr<Collider2D> Collider2D::create(ColliderType2D const collider_typ
     return collider_2d;
 }
 
+std::shared_ptr<Collider2D> Collider2D::create(float const width, float const height, bool const is_static)
+{
+    auto collider_2d = std::make_shared<Collider2D>(AK::Badge<Collider2D> {}, width, height, is_static);
+    return collider_2d;
+}
+
 Collider2D::Collider2D(AK::Badge<Collider2D>, ColliderType2D const collider_type, float const radius, bool const is_static) : m_radius(radius),
     m_is_static(is_static), m_collider_type(collider_type)
 {
@@ -29,6 +35,11 @@ Collider2D::Collider2D(AK::Badge<Collider2D>, ColliderType2D const collider_type
 
 Collider2D::Collider2D(AK::Badge<Collider2D>, ColliderType2D const collider_type, glm::vec2 const bounds_dimensions, bool const is_static)
     : m_bounds_dimensions(bounds_dimensions), m_is_static(is_static), m_collider_type(collider_type)
+{
+}
+
+Collider2D::Collider2D(AK::Badge<Collider2D>, float const width, float const height, bool const is_static) : m_is_static(is_static),
+    m_collider_type(ColliderType2D::Rectangle), m_width(width), m_height(height)
 {
 }
 
@@ -162,4 +173,105 @@ void Collider2D::set_static(bool const value)
     }
 
     m_is_static = value;
+}
+
+bool Collider2D::overlaps(Collider2D const& other) const
+{
+    return overlaps_one_way(other) && other.overlaps_one_way(*this);
+}
+
+void Collider2D::update()
+{
+    Component::update();
+
+    // NOTE+FIXME: destroy_immediate might break assumption that entity is not null, it probably should not do that.
+    if (entity != nullptr)
+    {
+        glm::vec2 const position = AK::convert_3d_to_2d(entity->transform->get_position());
+        float const angle = glm::radians(entity->transform->get_euler_angles().y);
+        compute_axes(position, angle);
+    }
+}
+
+void Collider2D::start()
+{
+    Component::start();
+
+    set_can_tick(true);
+    compute_axes({ 0.0f, 0.0f }, 0.0f);
+}
+
+bool Collider2D::overlaps_one_way(Collider2D const& other) const
+{
+    for (u32 a = 0; a < m_axes.size(); ++a)
+    {
+        float t = glm::dot(other.m_corners[0], m_axes[a]);
+
+        // Find the extent of box 2 on axis a.
+        float t_min = t;
+        float t_max = t;
+
+        for (u32 c = 1; c < m_corners.size(); ++c)
+        {
+            t = glm::dot(other.m_corners[c], m_axes[a]); // other.corner[c].dot(axis[a]);
+
+            if (t < t_min)
+            {
+                t_min = t;
+            }
+            else if (t > t_max)
+            {
+                t_max = t;
+            }
+        }
+
+        // We have to subtract off the origin.
+
+        // See if [t_min, t_max] intersects [0, 1]
+        if (t_min > 1 + m_origins[a] || t_max < m_origins[a])
+        {
+            // There was no intersection along this dimension;
+            // the boxes cannot possibly overlap.
+            return false;
+        }
+    }
+
+    // There was no dimension along which there is no intersection.
+    // Therefore the boxes overlap.
+    return true;
+}
+
+// NOTE: Should be called everytime the position has changed.
+//       Currently we just update it every frame.
+void Collider2D::compute_axes(glm::vec2 const& center, float const angle)
+{
+    // Create a 3D rotation matrix around the y-axis (y-up) and xz-plane.
+    glm::mat4 const rotation_matrix = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.0f, 1.0f, 0.0f));
+
+    // Rotate the 2D vectors
+    auto const x_3d = glm::vec3(rotation_matrix * glm::vec4(1, 0, 0, 1)); // Rotate the X-axis vector
+    auto const z_3d = glm::vec3(rotation_matrix * glm::vec4(0, 0, 1, 1)); // Rotate the Z-axis vector
+
+    // Convert the 3D vectors back to 2D by ignoring the y-component.
+    auto x = glm::vec2(x_3d.x, x_3d.z);
+    auto y = glm::vec2(z_3d.x, z_3d.z);
+
+    x *= m_width;
+    y *= m_height;
+
+    m_corners[0] = center - x - y;
+    m_corners[1] = center + x - y;
+    m_corners[2] = center + x + y;
+    m_corners[3] = center - x + y;
+
+    m_axes[0] = m_corners[1] - m_corners[0];
+    m_axes[1] = m_corners[3] - m_corners[0];
+
+    // Make the length of each axis 1/edge length so we know any
+    // dot product must be less than 1 to fall within the edge.
+    for (u32 a = 0; a < m_axes.size(); ++a)
+    {
+        m_axes[a] /= glm::pow(glm::length(m_axes[a]), 2); // axis[a].squared_length();
+        m_origins[a] = glm::dot(m_corners[0], m_axes[a]); // corner[0].dot(axis[a]);
+    }
 }
