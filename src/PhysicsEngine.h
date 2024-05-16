286df35b1049fa414fe2d3d2414ff69bfcbc711b
Mikolaj Przybylski <clovers02123@gmail.com>
2024-05-17 01:33:50 +0200

    Physics: Move computations from Collider2D into PhysicsEngine methods

--- src/PhysicsEngine.h
+++ src/PhysicsEngine.h
@@ -42,8 +42,20 @@ public:
     void emplace_collider(std::shared_ptr<Collider2D> const& collider);
     void remove_collider(std::shared_ptr<Collider2D> const& collider);
 
+    static bool compute_penetration(std::shared_ptr<Collider2D> const& collider, std::shared_ptr<Collider2D> const& other, glm::vec2& mtv);
+
 private:
     void solve_collisions() const;
+
+    static bool test_collision_rectangle_rectangle(Collider2D const& obb1, Collider2D const& obb2, glm::vec2& mtv);
+    static bool test_collision_circle_circle(Collider2D const& obb1, Collider2D const& obb2, glm::vec2& mtv);
+    static bool test_collision_circle_rectangle(Collider2D const& circle_collider, Collider2D const& rect_collider, glm::vec2& mtv);
+
+    static bool compute_penetration_circle_segment(glm::vec2 const& center, float const radius, glm::vec2 const& p1, glm::vec2 const& p2,
+                                                   glm::vec2& mtv);
+
+    static bool is_point_inside_obb(glm::vec2 const& point, std::array<glm::vec2, 4> const& rectangle_corners);
+
     std::vector<std::shared_ptr<Collider2D>> colliders = {};
     inline static std::shared_ptr<PhysicsEngine> m_instance;
 };
