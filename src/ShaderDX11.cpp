b62dd540120bf7764025007db0407db5caf66dcc
umbc1ok <umbcio@gmail.com>
2024-06-07 02:35:48 +0200

    Shaders: Don't recompile unchanged shaders

--- src/ShaderDX11.cpp
+++ src/ShaderDX11.cpp
@@ -4,11 +4,13 @@
 #include <d3d11.h>
 #include <d3dcommon.h>
 #include <d3dcompiler.h>
+#include <fstream>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.inl>
 #include <iostream>
 
+#include "AK/AK.h"
 #include "Renderer.h"
 #include "RendererDX11.h"
 
@@ -36,14 +38,19 @@ ShaderDX11::ShaderDX11(AK::Badge<ShaderFactory>, std::string const& vertex_path,
 
 void ShaderDX11::load_shader()
 {
+    HRESULT hr;
     // Load vertex shader
     ID3DBlob* vs_blob;
     {
         ID3DBlob* shader_compile_errors_blob;
 
         std::wstring const vertex_path_final = std::wstring(m_vertex_path.begin(), m_vertex_path.end());
-        HRESULT hr = D3DCompileFromFile(vertex_path_final.c_str(), nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, "vs_main", "vs_5_0", 0, 0,
-                                        &vs_blob, &shader_compile_errors_blob);
+        size_t size = 0;
+        char const* shader_source = read_hlsl_shader_from_file(m_vertex_path, &size);
+        hr = D3DPreprocess(shader_source, size, m_vertex_path.c_str(), nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, &vs_blob,
+                           &shader_compile_errors_blob);
+
+        delete[] shader_source;
 
         if (FAILED(hr))
         {
@@ -62,9 +69,26 @@ void ShaderDX11::load_shader()
             return;
         }
 
+        std::string const hash =
+            std::to_string(AK::murmur_hash(static_cast<u8*>(vs_blob->GetBufferPointer()), vs_blob->GetBufferSize(), 0));
+
+        if (!read_file_to_blob(m_vertex_path + hash + "vs_main", &vs_blob))
+        {
+            hr = D3DCompile(vs_blob->GetBufferPointer(), vs_blob->GetBufferSize(), nullptr, nullptr, nullptr, "vs_main", "vs_5_0", 0, 0,
+                            &vs_blob, &shader_compile_errors_blob);
+
+            if (FAILED(hr))
+            {
+                auto const error_string = static_cast<char const*>(shader_compile_errors_blob->GetBufferPointer());
+                std::cout << error_string << "\n";
+                return;
+            }
+
+            save_compiled_shader(m_vertex_path + hash + "vs_main", vs_blob);
+        }
+
         hr = RendererDX11::get_instance_dx11()->get_device()->CreateVertexShader(vs_blob->GetBufferPointer(), vs_blob->GetBufferSize(),
                                                                                  nullptr, &m_vertex_shader);
-
         if (FAILED(hr))
         {
             std::cout << "Error. Vertex shader creation failed."
@@ -79,9 +103,13 @@ void ShaderDX11::load_shader()
     {
         ID3DBlob* shader_compile_errors_blob;
 
-        std::wstring const pixel_path_final = std::wstring(m_fragment_path.begin(), m_fragment_path.end());
-        HRESULT hr = D3DCompileFromFile(pixel_path_final.c_str(), nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, "ps_main", "ps_5_0", 0, 0,
-                                        &ps_blob, &shader_compile_errors_blob);
+        auto const pixel_path_final = std::wstring(m_fragment_path.begin(), m_fragment_path.end());
+        size_t size = 0;
+        auto const shader_source = read_hlsl_shader_from_file(m_fragment_path, &size);
+        hr = D3DPreprocess(shader_source, size, m_fragment_path.c_str(), nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, &ps_blob,
+                           &shader_compile_errors_blob);
+
+        delete[] shader_source;
 
         if (FAILED(hr))
         {
@@ -99,6 +127,24 @@ void ShaderDX11::load_shader()
 
             std::cout << error_string << "\n";
             return;
+        }
+
+        std::string const hash =
+            std::to_string(AK::murmur_hash(static_cast<u8*>(ps_blob->GetBufferPointer()), ps_blob->GetBufferSize(), 0));
+
+        if (!read_file_to_blob(m_fragment_path + hash + "ps_main", &ps_blob))
+        {
+            hr = D3DCompile(ps_blob->GetBufferPointer(), ps_blob->GetBufferSize(), nullptr, nullptr, nullptr, "ps_main", "ps_5_0", 0, 0,
+                            &ps_blob, &shader_compile_errors_blob);
+
+            if (FAILED(hr))
+            {
+                auto const error_string = static_cast<char const*>(shader_compile_errors_blob->GetBufferPointer());
+                std::cout << error_string << "\n";
+                return;
+            }
+
+            save_compiled_shader(m_vertex_path + hash + "ps_main", ps_blob);
         }
 
         hr = RendererDX11::get_instance_dx11()->get_device()->CreatePixelShader(ps_blob->GetBufferPointer(), ps_blob->GetBufferSize(),
@@ -161,4 +207,104 @@ void ShaderDX11::set_mat4(std::string const& name, glm::mat4 const value) const
 i32 ShaderDX11::attach(char const* path, i32 type) const
 {
     return 0;
+}
+
+char* ShaderDX11::read_hlsl_shader_from_file(std::string const& path, size_t* p_size)
+{
+    std::ifstream file(path, std::ios::binary | std::ios::ate);
+
+    if (!file.is_open())
+    {
+        *p_size = 0;
+        return nullptr;
+    }
+
+    std::streamsize const file_size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    auto const buffer = new char[file_size + 1];
+
+    if (file.read(buffer, file_size))
+    {
+        buffer[file_size] = '\0'; // Null-terminate the string
+        *p_size = static_cast<size_t>(file_size);
+        return buffer;
+    }
+
+    delete[] buffer;
+    *p_size = 0;
+    return nullptr;
+}
+
+bool ShaderDX11::save_compiled_shader(std::string const& path, ID3DBlob* p_blob)
+{
+    size_t const last_slash_pos = path.find_last_of('/');
+    std::string const filename = path.substr(last_slash_pos + 1);
+    std::string const full_path = "./res/shaders/compiled/" + filename;
+
+    if (!p_blob)
+    {
+        std::cerr << "Invalid ID3DBlob pointer." << std::endl;
+        return false;
+    }
+
+    std::ofstream file(full_path, std::ios::binary);
+
+    if (!file.is_open())
+    {
+        std::cerr << "Failed to open file: " << full_path << std::endl;
+        return false;
+    }
+
+    file.write(static_cast<char const*>(p_blob->GetBufferPointer()), p_blob->GetBufferSize());
+
+    if (!file)
+    {
+        std::cerr << "Failed to write to file: " << full_path << std::endl;
+        return false;
+    }
+
+    file.close();
+
+    return true;
+}
+
+bool ShaderDX11::read_file_to_blob(std::string const& path, ID3DBlob** pp_blob)
+{
+    size_t const last_slash_pos = path.find_last_of('/');
+    std::string const filename = path.substr(last_slash_pos + 1);
+    std::string const full_path = "./res/shaders/compiled/" + filename;
+
+    if (!pp_blob)
+    {
+        return false;
+    }
+
+    std::ifstream file(full_path, std::ios::binary | std::ios::ate);
+
+    if (!file.is_open())
+    {
+        return false;
+    }
+
+    std::streamsize const file_size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    HRESULT const hr = D3DCreateBlob(file_size, pp_blob);
+
+    if (FAILED(hr))
+    {
+        std::cerr << "Failed to create blob." << std::endl;
+        return false;
+    }
+
+    if (!file.read(static_cast<char*>((*pp_blob)->GetBufferPointer()), file_size))
+    {
+        std::cerr << "Failed to read file: " << full_path << std::endl;
+        (*pp_blob)->Release();
+        *pp_blob = nullptr;
+        return false;
+    }
+
+    return true;
 }
