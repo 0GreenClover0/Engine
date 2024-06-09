fbc6a623301b92fdfe9e8bc945118155f87b41e3
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2024-06-09 15:42:40 +0200

    Particles: Remove global material + prepare for movement

--- src/Particle.h
+++ src/Particle.h
@@ -7,7 +7,9 @@ class Particle final : public Component
 {
 public:
     static std::shared_ptr<Particle> create();
+    static std::shared_ptr<Particle> create(float speed, float initial_alpha, float spawn_bounds);
     explicit Particle(AK::Badge<Particle>);
+    explicit Particle(AK::Badge<Particle>, float speed, float initial_alpha, float spawn_bounds);
 
     virtual void initialize() override;
 
@@ -15,10 +17,12 @@ public:
 
     virtual void draw_editor() override;
 
-    glm::vec4 color = {1.0f, 1.0f, 1.0f, 1.0f};
-
 private:
     void update_particle();
 
     ID3D11Buffer* m_constant_buffer_particle = nullptr;
+
+    glm::vec4 m_color = {0.0f, 0.0f, 1.0f, 1.0f};
+    float m_speed = 1.0f;
+    float m_spawn_bounds = 1.0f;
 };
