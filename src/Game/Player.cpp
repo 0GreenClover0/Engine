16f957bc4d7feaa9a889ae19c7de8fd2b9297b2a
Armez <zamylosz@gmail.com>
2024-06-13 22:53:19 +0200

    Player: Make the value calculations only when the game is running

--- src/Game/Player.cpp
+++ src/Game/Player.cpp
@@ -100,9 +100,10 @@ void Player::draw_editor()
         {
             LevelController::get_instance()->on_lighthouse_upgraded();
         }
+
+        ImGui::Text(("Ships Range: " + std::to_string(Player::get_instance()->range)).c_str());
+        ImGui::Text(("Ships Turn: " + std::to_string(Player::get_instance()->turn_speed)).c_str());
+        ImGui::Text(("Ships Additional Speed: " + std::to_string(Player::get_instance()->additional_ship_speed)).c_str());
+        ImGui::Text(("Pirates In Control: " + std::to_string(Player::get_instance()->pirates_in_control)).c_str());
     }
-    ImGui::Text(("Ships Range: " + std::to_string(Player::get_instance()->range)).c_str());
-    ImGui::Text(("Ships Turn: " + std::to_string(Player::get_instance()->turn_speed)).c_str());
-    ImGui::Text(("Ships Additional Speed: " + std::to_string(Player::get_instance()->additional_ship_speed)).c_str());
-    ImGui::Text(("Pirates In Control: " + std::to_string(Player::get_instance()->pirates_in_control)).c_str());
 }
