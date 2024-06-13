39a6f67a828da9b2fb62b7ebc5745047e8c9920a
Armez <zamylosz@gmail.com>
2024-06-13 22:49:08 +0200

    GameController: Add de/spawning scenes while moving to next level

--- src/Game/GameController.cpp
+++ src/Game/GameController.cpp
@@ -1,12 +1,14 @@
 #include <GLFW/glfw3.h>
 #include <imgui.h>
+#include <imgui_extensions.h>
 
 #include "AK/Math.h"
 #include "Entity.h"
 #include "GameController.h"
 #include "Globals.h"
 #include "Input.h"
-#include <imgui_extensions.h>
+#include "LevelController.h"
+#include "SceneSerializer.h"
 
 std::shared_ptr<GameController> GameController::create()
 {
@@ -39,6 +41,12 @@ void GameController::uninitialize()
 
 void GameController::awake()
 {
+    std::ranges::reverse(levels_order);
+
+    std::string const level = levels_order.back();
+    levels_order.pop_back();
+    current_scene = SceneSerializer::load_prefab(level);
+
     set_can_tick(true);
 }
 
@@ -52,17 +60,23 @@ void GameController::update()
     if (m_move_to_next_scene_counter < 1.0f)
     {
         m_move_to_next_scene_counter += 0.01f;
+
+        update_scenes_position();
     }
     else
     {
+        update_scenes_position();
+
+        current_scene.lock()->destroy_immediate();
+
+        current_scene = next_scene;
+        current_scene.lock()->transform->set_local_position({0.0f, 0.0f, 0.0f});
+
+        next_scene = {};
+
         m_move_to_next_scene_counter = 0.0f;
         m_move_to_next_scene = false;
     }
-
-    current_scene.lock()->transform->set_local_position(
-        glm::vec3(0.0f - ease_in_out_cubic(m_move_to_next_scene_counter) * 18.0f, 0.0f, 0.0f));
-    next_scene.lock()->transform->set_local_position(
-        glm::vec3(18.0f - ease_in_out_cubic(m_move_to_next_scene_counter) * 18.0f, 0.0f, 0.0f));
 }
 
 void GameController::draw_editor()
@@ -74,11 +88,28 @@ void GameController::draw_editor()
 
     if (ImGui::Button("Move"))
     {
+        LevelController::get_instance()->destroy_immediate();
+
+        next_scene = SceneSerializer::load_prefab(levels_order.back());
+
         m_move_to_next_scene = true;
     }
+}
+
+bool GameController::is_moving_to_next_scene() const
+{
+    return m_move_to_next_scene;
 }
 
 float GameController::ease_in_out_cubic(float const x) const
 {
     return x < 0.5f ? 4.0f * x * x * x : 1.0f - std::pow(-2.0f * x + 2.0f, 3.0f) / 2.0f;
+}
+
+void GameController::update_scenes_position() const
+{
+    current_scene.lock()->transform->set_local_position(
+        glm::vec3(0.0f - ease_in_out_cubic(m_move_to_next_scene_counter) * 18.0f, 0.0f, 0.0f));
+    next_scene.lock()->transform->set_local_position(
+        glm::vec3(18.0f - ease_in_out_cubic(m_move_to_next_scene_counter) * 18.0f, 0.0f, 0.0f));
 }
