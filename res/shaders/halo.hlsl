147dff00d51b140e3c1fc21a05bf202acdc83b95
umbc1ok <umbcio@gmail.com>
2024-06-18 22:53:35 +0200

    Shaders: Add separate glow function

--- res/shaders/halo.hlsl
+++ res/shaders/halo.hlsl
@@ -1,13 +1,10 @@
+#include "lighting_calculations.hlsl"
+
 cbuffer object_buffer : register(b0)
 {
     float4x4 projection_view_model;
     float4x4 world;
 };
-
-cbuffer camera_buffer : register(b2)
-{
-    float3 camera_pos;
-}
 
 struct vs_input
 {
@@ -37,30 +34,22 @@ vs_output vs_main(vs_input input)
     return output;
 }
 
+float4 glow(float3 color, float3 world_pos, float3 world_normal)
+{
+    float3 view_dir = normalize(camera_pos.xyz - world_pos.xyz);
+    color.xyz = calculate_directional_light(directional_light, world_normal, view_dir, color.xyz, world_pos, true);
+
+    float normal_dot_view = dot(world_normal, view_dir);
+
+    float halo_strength = pow(1.1f - abs(normal_dot_view), 1.5f);
+
+    float3 halo_color = float3(1.0f, 1.0f, 1.0f) * halo_strength;
+    return float4((color + halo_color), 1.0f);
+}
+
 float4 ps_main(vs_output input) : SV_TARGET
 {
-    float4 tex_color = float4(0.0f, 0.0f, 0.0f, 1.0f); // Initialize with black color and alpha 1.0
+    float4 tex_color = obj_texture.Sample(obj_sampler_state, input.uv);
 
-    float3 view_dir = normalize(camera_pos.xyz - input.world_pos.xyz); 
-
-    float normal_dot_view = dot(normalize(input.world_normal), view_dir); // Dot product of normalized vectors
-
-    float halo_strength = pow(1.7f - abs(normal_dot_view), 3.9f);
-
-    float4 halo_color = float4(1.0f, 0.5f, 0.5f, 1.0f) * halo_strength; // White halo color modulated by strength
-
-    // Combine tex_color and halo_color, ensuring the alpha value is 1.0 in the output
-    float4 color = tex_color + halo_color;
-    return float4(color.xyz, color.w / 100.0f);
-    // float4 tex_color = obj_texture.Sample(obj_sampler_state, input.uv);
-
-    // float3 view_dir = normalize(camera_pos.xyz - input.world_pos.xyz); 
-
-    // float normal_dot_view = dot(input.world_normal, view_dir);
-
-    // float halo_strength = pow(1.2 - abs(normal_dot_view), 3.0);
-
-    // float4 halo_color = float4(1.0, 0.0, 0.0, 1.0) * halo_strength;
-
-    // return tex_color + halo_color;
+    return glow(tex_color.xyz, input.world_pos, input.world_normal);
 }
