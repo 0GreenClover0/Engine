3605c29647b0abff50880ff4e7ffff07665434f2
Mikolaj Przybylski <clovers02123@gmail.com>
2024-06-19 21:31:55 +0200

    UI: Correct serialized render orders+set casts_shadows to false on text

--- src/ScreenText.cpp
+++ src/ScreenText.cpp
@@ -18,7 +18,8 @@
 std::shared_ptr<ScreenText> ScreenText::create()
 {
     auto const ui_shader = ResourceManager::get_instance().load_shader("./res/shaders/ui.hlsl", "./res/shaders/ui.hlsl");
-    auto const ui_material = Material::create(ui_shader, Renderer::ui_render_order + 2);
+    auto const ui_material = Material::create(ui_shader, Renderer::ui_render_order + 3);
+    ui_material->casts_shadows = false;
 
     auto text = std::make_shared<ScreenText>(AK::Badge<ScreenText> {}, ui_material, "Example text", glm::vec2(0, 0), 40, 0xff0099ff,
                                              FW1_RESTORESTATE | FW1_CENTER | FW1_VCENTER);
