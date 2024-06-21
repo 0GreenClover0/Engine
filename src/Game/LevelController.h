8ae7b6c7f2a9eada02d1a6f1287caff16c074789
Armez <zamylosz@gmail.com>
2024-06-21 23:25:19 +0200

    LevelController: Add ship in flash collider tutorial action

--- src/Game/LevelController.h
+++ src/Game/LevelController.h
@@ -19,6 +19,7 @@ enum class TutorialProgressAction
     ShipEnteredPort,
     ShipEnteredControl,
     ShipDestroyed,
+    ShipInFlashCollider,
     KeeperLeavedLighthouse,
     KeeperEnteredLighthouse,
     KeeperEnteredPort,
