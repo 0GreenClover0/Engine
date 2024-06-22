ad905fe65b7d26669b0c178abfcdad9be5125c7e
Armez <zamylosz@gmail.com>
2024-06-22 19:57:22 +0200

    EndScreen: Add updating background with fail / win

--- src/Game/EndScreen.h
+++ src/Game/EndScreen.h
@@ -8,6 +8,7 @@ class EndScreen final : public Component
 {
 public:
     static std::shared_ptr<EndScreen> create();
+    static std::shared_ptr<EndScreen> create(bool const is_failed);
 
     explicit EndScreen(AK::Badge<EndScreen>);
 
@@ -16,11 +17,17 @@ public:
 #if EDITOR
     virtual void draw_editor() override;
 #endif
+    void update_background();
     void update_screen_position() const;
+
+    bool is_failed = false;
 
 private:
     static float ease_out_back(float x);
 
     bool m_is_in_screen = false;
     float m_appear_counter = 0.0f;
+
+    std::string m_failed_background_path = "./res/textures/UI/end_screen_try_again.png";
+    std::string m_win_background_path = "./res/textures/UI/end_screen_level_completed.png";
 };
--- src/SceneSerializer.cpp
+++ src/SceneSerializer.cpp
@@ -424,6 +424,7 @@ void SceneSerializer::auto_serialize_component(YAML::Emitter& out, std::shared_p
         out << YAML::Key << "ComponentName" << YAML::Value << "EndScreenComponent";
         out << YAML::Key << "guid" << YAML::Value << endscreen->guid;
         out << YAML::Key << "custom_name" << YAML::Value << endscreen->custom_name;
+        out << YAML::Key << "is_failed" << YAML::Value << endscreen->is_failed;
         out << YAML::EndMap;
     }
     else if (auto const factory = std::dynamic_pointer_cast<class Factory>(component); factory != nullptr)
@@ -1605,6 +1606,10 @@ void SceneSerializer::auto_deserialize_component(YAML::Node const& component, st
         {
             auto const deserialized_component =
                 std::dynamic_pointer_cast<class EndScreen>(get_from_pool(component["guid"].as<std::string>()));
+            if (component["is_failed"].IsDefined())
+            {
+                deserialized_component->is_failed = component["is_failed"].as<bool>();
+            }
             deserialized_entity->add_component(deserialized_component);
             deserialized_component->reprepare();
         }
