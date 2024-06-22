081a7773ef622a1f7b40b03897aebc22b9dca855
Armez <zamylosz@gmail.com>
2024-06-22 20:16:49 +0200

    Prefabs: Add end screen prefab

--- src/Button.cpp
+++ src/Button.cpp
@@ -79,7 +79,8 @@ void Button::draw_editor()
 
     static float width = 1;
     static float height = 1;
-    static float result = 1;
+    static float result_width = 1;
+    static float result_height = 1;
     static float img_scale = 1;
 
     bool is_size_changed = false;
@@ -90,15 +91,18 @@ void Button::draw_editor()
     if (is_size_changed)
     {
         float x = (9 * width) / 16;
-        result = x / height;
+        float y = (16 * height) / 9;
+        result_width = x / height;
+        result_height = y / width;
     }
 
-    ImGui::Text(("Result: " + std::to_string(result)).c_str());
+    ImGui::Text(("Result width: " + std::to_string(result_width)).c_str());
+    ImGui::Text(("Result height: " + std::to_string(result_height)).c_str());
 
     if (ImGui::Button("Applay to width"))
     {
         glm::vec3 scale = entity->transform->get_local_scale();
-        entity->transform->set_local_scale({scale.x * result, scale.y, 1.0f});
+        entity->transform->set_local_scale({scale.x * result_width, scale.y, 1.0f});
     }
 
     ImGui::SameLine();
@@ -106,7 +110,7 @@ void Button::draw_editor()
     if (ImGui::Button("Applay to height"))
     {
         glm::vec3 scale = entity->transform->get_local_scale();
-        entity->transform->set_local_scale({scale.x, scale.y * result, 1.0f});
+        entity->transform->set_local_scale({scale.x, scale.y * result_height, 1.0f});
     }
 
     ImGui::InputFloat("Scale: ", &img_scale);
