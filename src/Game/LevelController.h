e317b81906cc3c9807f1145f183975a3f9713499
Armez <zamylosz@gmail.com>
2024-06-22 15:37:29 +0200

    LevelController: Add Pirate Destroy tutorial action

--- src/Game/LevelController.h
+++ src/Game/LevelController.h
@@ -20,6 +20,7 @@ enum class TutorialProgressAction
     ShipEnteredControl,
     ShipDestroyed,
     ShipInFlashCollider,
+    PirateDestroyed,
     KeeperLeftLighthouse,
     KeeperEnteredLighthouse,
     KeeperEnteredPort,
