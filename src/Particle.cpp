c6de7f4ca94e239e7ae87c24559f57a7fde87dd7
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2024-06-25 20:31:20 +0200

    Particles: Apply rotation correctly in update and on awake

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -73,7 +73,10 @@ void Particle::awake()
                                            AK::random_float(-m_spawn_bounds, m_spawn_bounds)});
     if (rotate)
     {
-        entity->transform->set_euler_angles({0, 0, AK::random_float(0.0f, 360.0f)});
+        auto const rot = entity->transform->get_euler_angles();
+        glm::vec3 const d_rot = {0.0f, 0.0f, AK::random_float(0.0f, 360.0f)};
+
+        entity->transform->set_euler_angles(rot + d_rot);
     }
 
     m_rotation_direction = AK::random_bool() ? 1.0f : -1.0f;
@@ -218,6 +221,14 @@ void Particle::move() const
     }
 
     entity->transform->set_position(new_position);
+
+    if (rotate)
+    {
+        auto const rot = entity->transform->get_euler_angles();
+        glm::vec3 const d_rot = {0.0f, 0.0f, delta_time * m_velocity.y * 50.0f};
+
+        entity->transform->set_euler_angles(rot + d_rot * m_rotation_direction);
+    }
 }
 
 void Particle::interpolate_color()
