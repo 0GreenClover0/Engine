ca4f2837441ee50b6ac9b3a1fb18839877cfa1c2
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2024-06-27 01:29:10 +0200

    Particles: Make fish more readable, slower and cleaner

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -236,7 +236,7 @@ void Particle::move() const
         new_position = glm::mix(entity->transform->parent.lock()->get_position(), m_customer_group_position, t) + m_fish_offset;
 
         auto const rot = entity->transform->get_euler_angles();
-        glm::vec3 const d_rot = {0.0f, 0.0f, delta_time * m_velocity.y * 850.0f};
+        glm::vec3 const d_rot = {0.0f, 0.0f, delta_time * m_velocity.y * 600.0f};
         entity->transform->set_euler_angles(rot + d_rot * m_rotation_direction);
 
         break;
